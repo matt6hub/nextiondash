4472a1a07817de154370239a5e59e608158d264c
matt6hub <matteopiomuscat@gmail.com>
2025-01-22 04:43:26 +0100

    Starting to implement MEMS19 code

--- nextiondash-kline/src/main.h
+++ nextiondash-kline/src/main.h
@@ -12,6 +12,11 @@
 
 #define IF_NEXTION "/dev/ttyS0" //Interface name for nextion testing interface
 
+//MEMS19 Commands
+unsigned char ecuSpecificInitCommand = 0x7C;
+unsigned char ecuWokeResponse[3] = {0x55, 0x76, 0x83};
+unsigned char ecuSpecificInitResponse[4] = {0x55, 0x76, 0x83, 0xE9};
+
 //Vehicle Telemetry Variables
 float rpm, tps, iacv, spd, afr, iat, clt, battery;
 
